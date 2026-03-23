a5a5a205a4d8d61cde33c9d5bdaf7954bf162a94
IXsushanthreddy <saisushanth.reddy@innovatechs.com>
2026-03-23 14:52:30 +0530

    Add SpringAssignment004 project

---
+++ SpringAssignment004/src/main/java/com/sushanthreddy/springboot/springassignment004/SpringAssignment004Application.java
@@ -0,0 +1,13 @@
+package com.sushanthreddy.springboot.springassignment004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignment004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAssignment004Application.class, args);
+    }
+
+}
