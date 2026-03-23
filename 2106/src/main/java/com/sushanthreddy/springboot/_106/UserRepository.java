d20351b7af3db906a7ffd1958a7b6342297f44c8
IXsushanthreddy <saisushanth.reddy@innovatechs.com>
2026-03-23 16:14:43 +0530

    Delete 2106 directory

--- 2106/src/main/java/com/sushanthreddy/springboot/_106/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sushanthreddy.springboot._106;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
